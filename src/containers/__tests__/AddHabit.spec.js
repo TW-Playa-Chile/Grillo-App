b4bf7b44096bd86372c55c956d7956edd2b4377b
Javier Villalobos A <jvillalo@thoughtworks.com>
2017-10-28 14:42:55 -0300

    clean code

--- src/containers/__tests__/AddHabit.spec.js
+++ src/containers/__tests__/AddHabit.spec.js
@@ -3,43 +3,45 @@ import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import configureStore from 'redux-mock-store';
 import Immutable from 'immutable';
-Enzyme.configure({ adapter: new Adapter() });
 import AddHabit from './../AddHabit';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 
 describe('components', () => {
-    const initialState = Immutable.Map({
-        habits: [
-            {name: 'no comeras', startDate: '2017-10-10T12:59-0500'},
-            {name: 'no beberas', startDate: '2017-10-14T12:59-0500'},
-            {name: 'no bailaras', startDate: '2017-10-16T12:59-0500'}
-        ]
-    });
-    const mockStore = configureStore();
-    let store, wrapper;
+  const initialState = Immutable.Map({
+    habits: [
+      { name: 'no comeras', startDate: '2017-10-10T12:59-0500' },
+      { name: 'no beberas', startDate: '2017-10-14T12:59-0500' },
+      { name: 'no bailaras', startDate: '2017-10-16T12:59-0500' }
+    ]
+  });
+  const mockStore = configureStore();
+  let store;
+  let wrapper;
 
-    beforeEach(() => {
-        store = mockStore(initialState);
-        wrapper = shallow(<AddHabit store={store} navigation={{goBack: jest.fn()}} />).dive().instance();
-    });
-
-    it('should set an error when habit name is absent', () => {
-      wrapper.enterHabit("");
-      wrapper.addHabitToStore();
-      expect( wrapper.state.habitError).toBe(true);
-    });
-
-    it('should pass if habit has a name', () => {
-      wrapper.enterHabit("A");
-      wrapper.addHabitToStore();
-      expect( wrapper.state.habitError ).toBe(false);
-    });
-
-    it('should set an error if we pass multiple spaces', () => {
-      wrapper.enterHabit("      ");
-      wrapper.addHabitToStore();
-      expect( wrapper.state.habitError ).toBe(true);
+  beforeEach(() => {
+    store = mockStore(initialState);
+    wrapper = shallow(<AddHabit store={store} navigation={{ goBack: jest.fn() }} />)
+      .dive().instance();
   });
 
+  it('should set an error when habit name is absent', () => {
+    wrapper.enterHabit('');
+    wrapper.addHabitToStore();
+    expect(wrapper.state.habitError).toBe(true);
+  });
+
+  it('should pass if habit has a name', () => {
+    wrapper.enterHabit('A');
+    wrapper.addHabitToStore();
+    expect(wrapper.state.habitError).toBe(false);
+  });
+
+  it('should set an error if we pass multiple spaces', () => {
+    wrapper.enterHabit('      ');
+    wrapper.addHabitToStore();
+    expect(wrapper.state.habitError).toBe(true);
+  });
 });
-;
+
