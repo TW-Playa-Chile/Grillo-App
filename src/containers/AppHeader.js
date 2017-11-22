3932d9b310a349d37acbc9a3c154dc13a7019f15
Nicolas Justiniano <n.justiniano@gmail.com>
2017-11-22 16:54:08 -0300

    PG-000 reducing appheader

--- src/containers/AppHeader.js
+++ src/containers/AppHeader.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
 import { Header, Icon } from 'react-native-elements';
 import * as HabitActions from '../actions/habits';
 import { COLOR_PRIMARY, FONT_BOLD } from './../styles/common';
@@ -16,36 +15,17 @@ export default class AppHeader extends Component {
     super(props);
 
     const { cleanHabits } = props;
-    this.state = {
-      counter: 0,
-    }
 
     this.cleanHabits = () => cleanHabits();
-  }
-
-  cleanHabits = () => {
-    this.setState({counter: this.state.counter + 1}, () => {
-      if (this.state.counter > 6) {
-        this.cleanHabits();
-      }
-    });
-  }
-
-  deleteButton() {
-    return <Icon
-              name = "delete"
-              onPress = {this.cleanHabits}
-              color = "#FFFFFF"
-            />
   }
 
   render() {
     return (
       <Header
         backgroundColor={COLOR_PRIMARY}
-        centerComponent={{ text: 'Pepe Grillo', style: { ...FONT_BOLD, color: '#fff' } }}
-        rightComponent={this.deleteButton()}
+        centerComponent={{ text: 'Pepe Grillo', style: { ...FONT_BOLD, color: '#FFFFFF' } }}
+        rightComponent={<Icon name="delete" onPress={this.cleanHabits} color="#FFFFFF" />}
       />
-    )
+    );
   }
 }
