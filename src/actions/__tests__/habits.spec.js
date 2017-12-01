889b2c41eea414f2d9fb2565d6b06b6f4326a095
Javier Villalobos A <jvillalo@thoughtworks.com>
2017-12-01 12:42:39 -0300

    {PG-021d} {Cristina, Javier} Se realizan ajustes a la descripción de las pruebas

--- src/actions/__tests__/habits.spec.js
+++ src/actions/__tests__/habits.spec.js
@@ -14,12 +14,12 @@ describe('habit actions', () => {
     expect(actions.addHabit('Fumar oregano').payload.name).toBe('Fumar oregano');
   });
 
-  it('should get type "see_habit" when invoque "seeHabit" action', () => {
+  it('should receive action with see_habit type when "seeHabit" action is invoqued', () => {
     const habit = new Habit('Fumar oregano');
     expect(actions.seeHabit(habit).type).toBe('see_habit');
   });
 
-  it('should get habit with "seen" property true when invoque "seeHabit" action', () => {
+  it('should receive habit with seen property equals true when "seeHabit" action is invoqued', () => {
     const habit = new Habit('Fumar oregano');
     expect(actions.seeHabit(habit).payload.seen).toBe(true);
   });
